24856cd94958c99f4e3bda9d04d7cc72303e56e0
blazej <blazej.uliasz2004@gmail.com>
2025-09-20 22:57:54 +0200

    first commit

---
+++ src/main/java/pl/bliasz/prescription_web_service/PrescriptionWebServiceApplication.java
@@ -0,0 +1,14 @@
+package pl.bliasz.prescription_web_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrescriptionWebServiceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PrescriptionWebServiceApplication.class, args);
+	}
+
+}
