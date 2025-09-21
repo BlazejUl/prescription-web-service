4427f9bcac09201aeff32c4ec017281993f92075
blazej <blazej.uliasz2004@gmail.com>
2025-09-21 16:58:30 +0200

    added doctor entity

---
+++ src/main/java/pl/bliasz/prescription_web_service/doctor/Doctor.java
@@ -0,0 +1,52 @@
+package pl.bliasz.prescription_web_service.doctor;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Doctor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String pwz;
+    private String fullName;
+    private String password;
+
+    public Doctor() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPwz() {
+        return pwz;
+    }
+
+    public void setPwz(String pwz) {
+        this.pwz = pwz;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
