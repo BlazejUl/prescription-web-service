40efcda0705f92f8e635b0e28b527a201ae442ab
blazej <blazej.uliasz2004@gmail.com>
2025-09-23 16:40:35 +0200

    made Prescription entity

--- src/main/java/pl/bliasz/prescription_web_service/prescription/Prescription.java
+++ src/main/java/pl/bliasz/prescription_web_service/prescription/Prescription.java
@@ -1,4 +1,71 @@
 package pl.bliasz.prescription_web_service.prescription;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.sql.Date;
+
+@Entity
 public class Prescription {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer Id;
+    private String drugName;
+    private Date startDate;
+    private Date endDate;
+    private Integer patientId;
+    private Integer doctorId;
+
+    public Prescription() {
+    }
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public String getDrugName() {
+        return drugName;
+    }
+
+    public void setDrugName(String drugName) {
+        this.drugName = drugName;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Integer getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(Integer patientId) {
+        this.patientId = patientId;
+    }
+
+    public Integer getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(Integer doctorId) {
+        this.doctorId = doctorId;
+    }
 }
