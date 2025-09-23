70ed75b84551006add082f91dcfdb0f859d6c908
blazej <blazej.uliasz2004@gmail.com>
2025-09-23 16:44:17 +0200

    made getAll

--- src/main/java/pl/bliasz/prescription_web_service/prescription/PrescriptionController.java
+++ src/main/java/pl/bliasz/prescription_web_service/prescription/PrescriptionController.java
@@ -1,4 +1,19 @@
 package pl.bliasz.prescription_web_service.prescription;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class PrescriptionController {
+    PrescriptionService prescriptionService;
+
+    public PrescriptionController(PrescriptionService prescriptionService) {
+        this.prescriptionService = prescriptionService;
+    }
+
+    @GetMapping("/prescription")
+    public ResponseEntity<Iterable<Prescription>> getAllPrescriptions(){
+        return ResponseEntity.ok(prescriptionService.getAll());
+    }
 }
--- src/main/java/pl/bliasz/prescription_web_service/prescription/PrescriptionService.java
+++ src/main/java/pl/bliasz/prescription_web_service/prescription/PrescriptionService.java
@@ -1,4 +1,16 @@
 package pl.bliasz.prescription_web_service.prescription;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class PrescriptionService {
+    PrescriptionRepository prescriptionRepository;
+
+    public PrescriptionService(PrescriptionRepository prescriptionRepository) {
+        this.prescriptionRepository = prescriptionRepository;
+    }
+
+    public Iterable<Prescription> getAll(){
+        return prescriptionRepository.findAll();
+    }
 }
