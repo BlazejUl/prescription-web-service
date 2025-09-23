d754234016002ff4f5756d0c4f61a3b51d2c152b
blazej <blazej.uliasz2004@gmail.com>
2025-09-23 16:20:27 +0200

    ade patient getAll

--- src/main/java/pl/bliasz/prescription_web_service/patient/PatientController.java
+++ src/main/java/pl/bliasz/prescription_web_service/patient/PatientController.java
@@ -1,8 +1,20 @@
 package pl.bliasz.prescription_web_service.patient;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PatientController {
 
+    PatientService patientService;
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping("/patients")
+    public ResponseEntity<Iterable<Patient>> getAllPatients(){
+        return ResponseEntity.ok(patientService.getAll());
+    }
 }
--- src/main/java/pl/bliasz/prescription_web_service/patient/PatientService.java
+++ src/main/java/pl/bliasz/prescription_web_service/patient/PatientService.java
@@ -1,4 +1,13 @@
 package pl.bliasz.prescription_web_service.patient;
 
 public class PatientService {
+    PatientRepository patientRepository;
+
+    public PatientService(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    public Iterable<Patient> getAll(){
+        return patientRepository.findAll();
+    }
 }
