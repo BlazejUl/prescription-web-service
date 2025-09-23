e1cc1c90607a5a6089aa5245fb2526b6254e768f
blazej <blazej.uliasz2004@gmail.com>
2025-09-23 16:15:26 +0200

    ade Patinet entity class

--- src/main/java/pl/bliasz/prescription_web_service/patient/Patient.java
+++ src/main/java/pl/bliasz/prescription_web_service/patient/Patient.java
@@ -1,7 +1,51 @@
 package pl.bliasz.prescription_web_service.patient;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String pesel;
+    private String password;
+
+    public Patient() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPesel() {
+        return pesel;
+    }
+
+    public void setPesel(String pesel) {
+        this.pesel = pesel;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "id=" + id +
+                ", pesel='" + pesel + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
